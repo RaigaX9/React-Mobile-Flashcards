ace473558ed4310f38bf12dcdaba1efd7c75eaff
ZetaX9 <nishad3245@gmail.com>
2018-09-04 23:29:19 -0400

    Updated the status bar

--- mobile-flashcards/components/Statusbar.js
+++ mobile-flashcards/components/Statusbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Constants } from 'expo';
 import { View, StatusBar } from 'react-native';
 
-const Statusbar = ({ backgroundColor, ...props}) => {
+const Statusbar = () => {
     return (
         <View style={{ backgroundColor: '#000000', height: 25 }}>
             <StatusBar />
